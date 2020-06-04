46d97fd23f04dfdbc7db823f6f971c021cbbd6a1
jalayna <jalaynatipantiza@gmail.com>
2020-06-04 01:05:49 +0000

    implemented  word search solver

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    let verticalJoin = ['','','','','','','',''];
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        } 
     }
+
+    for (let i = 0; i < horizontalJoin.length; i++) {
+        for (j = 0; j < horizontalJoin[i].length; j++) {
+            verticalJoin[j] += horizontalJoin[i][j];
+        }
+    }
+    // console.log(verticalJoin);
+    // console.log(verticalJoin.map(ls => ls.join('')));
+
+
+
+    for (const l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
